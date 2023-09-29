ad5e48799ba4e8178a06d1e5d0a23fbfc4e381a2
Sam <100703206+SamChrp@users.noreply.github.com>
2023-09-29 10:41:59 +0200

    wip : display movies

---
+++ src/components/moviesHomePageView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted, ref, toRaw } from "vue";
+import axios  from "axios";
+
+let data = ref("");
+
+onMounted(async () => {
+  const response = await axios.get(
+      "https://127.0.0.1:8000/api/movies",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+  );
+  data.value = response.data;
+});
+</script>
+
+<template>
+  <header class="header-home">
+    <h1>ACCUEIL</h1>
+  </header>
+  <div v-for="film in data">{{ film.title }} / {{ film.releaseDate }} / {{ film.description }} / {{ film.duration }}</div>
+
+</template>
+
+<style scoped>
+
+</style>
