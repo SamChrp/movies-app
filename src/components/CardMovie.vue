df2828daf70e873314bacde518c2bad4e3d726cd
Sam <100703206+SamChrp@users.noreply.github.com>
2023-10-16 11:38:52 +0200

    wip : nav-bar

---
+++ src/components/CardMovie.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="card-box">
+
+  </div>
+</template>
+
+<style scoped>
+  .card-box {
+    width: 33%;
+    height: 20rem;
+    background-color: whitesmoke;
+  }
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="nav-bar-container">
+    <div class="nav-bar-links"></div>
+    <div>Home</div>
+    <div>Movies</div>
+    <div>Actors</div>
+    <div>Categories</div>
+  </div>
+</template>
+
+<style scoped>
+  .nav-bar-container {
+    margin: 0;
+    width: 100%;
+    height: 5rem;
+    background-color: pink;
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
