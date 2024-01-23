6adc9eaf3517bee2749a1962068e890a3cf420b6
Sam <100703206+SamChrp@users.noreply.github.com>
2024-01-23 16:31:40 +0100

    wip

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -1,18 +1,26 @@
 <script setup>
+import {defineProps} from "vue";
 
+const {film} = defineProps({
+  film: Object
+});
 
 </script>
 
 <template>
   <div class="card-box">
-
+    <h1>{{film.title}}</h1>
+    <p>{{film.releaseDate}}</p>
+    <p>{{film.description}}</p>
+    <p>{{film.duration}}</p>
   </div>
 </template>
 
 <style scoped>
-  .card-box {
-    width: 33%;
-    height: 20rem;
-    background-color: whitesmoke;
-  }
+.card-box {
+  width: 25%;
+  height: 20rem;
+  background-color: whitesmoke;
+  margin: 1rem;
+}
 </style>
